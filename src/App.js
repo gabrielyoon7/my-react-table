1278b76e9b96dd656f75d5d815a0deed5bfe18b8
gabrielyoon7 <gabrielyoon7@gmail.com>
2023-04-14 14:15:07 +0900

    feat: useTable 예제 추가

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import Table from './component/Table';
+import curriculum from './data/curriculum';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div style={{ minWidth: '800px' }}>
+        <Table data={curriculum} rowsPerPage={10} />
+      </div>
+    </>
   );
 }
 
